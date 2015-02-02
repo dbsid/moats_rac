a841447b834e458a6428cd516f53337f14a73ce0
sidney <sidney.chen@oracle.com>
2015-02-02 11:00:04 +0800

    initial commit

---
+++ moats_settings.sql
@@ -0,0 +1,7 @@
+
+set arrays 80
+set lines 175
+set head off
+set tab off
+set pages 0
+set serveroutput on format wrapped
---
+++ q.sql
@@ -0,0 +1,2 @@
+set arrays 80 lines 175 head off tab off pages 0
+SELECT /*+ no_monitor */* FROM TABLE(moats.top(5));
